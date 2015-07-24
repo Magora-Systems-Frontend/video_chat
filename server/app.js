6568f780c16d0f09db4ed311311049c44341a678
magoragit <support@magora.ru>
2015-07-24 08:54:48 +0600

    rm#32838 Сервер часть видео чата added start script and config files

--- server/app.js
+++ server/app.js
@@ -1,13 +1,82 @@
-var express = require('express');
-var app = express();
+(function () {
+    'use strict';
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+    var requireDir = require('require-dir'),
+        express = require('express'),
+        merge = require('deepmerge'),
+        argv = require('minimist')(process.argv.slice(2)),
+        mongoose = require('mongoose'),
+        configs = requireDir('./config') || {},
+        appConfig = {},
+        app = express();
 
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+    app.set('env', argv.p ? 'prod' : 'dev');
 
-    console.log('Example app listening at http://%s:%s', host, port);
-});
+    for (var key in configs) {
+        if (configs.hasOwnProperty(key)) {
+            if (configs[key][app.get('env')]) {
+                appConfig[key] = merge(configs[key]['default'], configs[key][app.get('env')])
+            } else {
+                appConfig[key] = configs[key]['default']
+            }
+        }
+    }
+
+    app.set('config', appConfig);
+
+    function mongoConnect() {
+        mongoose.connect('mongodb://' + appConfig.mongodb.host + '/' + appConfig.mongodb.database, appConfig.mongodb.options)
+    }
+
+    mongoConnect();
+
+    var models = requireDir('./models');
+
+    mongoose.connection.on('error', console.log);
+    mongoose.connection.on('disconnected', mongoConnect);
+
+    app.use(function (req, res, next) {
+
+        var cors = req.app.get('config').cors;
+
+        Object.keys(cors).forEach(function (key) {
+            res.header(key, cors[key]);
+        });
+
+        next();
+    });
+
+
+    var controllers = requireDir('./controllers');
+
+    Object.keys(appConfig.route).forEach(function (pattern) {
+        var params = appConfig.route[pattern],
+            method,
+            uri;
+
+        pattern = pattern.split(' ');
+
+        if (pattern.length > 1) {
+            method = pattern[0].toLowerCase();
+            uri = pattern[1];
+        }
+        else {
+            uri = pattern[0];
+            method = 'all';
+        }
+
+        app[method](uri, function (req, res) {
+            if (!params.politics
+                && controllers[params.controller + 'Controller']
+                && controllers[params.controller + 'Controller']['action' + params.action]
+            ) {
+                controllers[params.controller + 'Controller']['action' + params.action](req, res)
+            }
+        });
+    });
+
+    app.listen(appConfig.server.port);
+    console.log('Server running on ' + appConfig.server.port + ' port, env "' + app.get('env') + '"');
+
+
+})();
