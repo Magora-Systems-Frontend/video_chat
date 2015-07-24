753f4d96c0cf6fde8a7042c6378a1233b45f7f97
Prokashev Vadim <prokashev@magora.systems>
2015-07-24 09:04:07 +0600

    ~

--- gulp/tasks/fonts.js
+++
@@ -1,4 +0,0 @@
-gulp.task('fonts', function () {
-    return gulp.src(config['fonts']['src'])
-        .pipe(gulp.dest(config['public'] + config['fonts']['dst']))
-});
--- gulp/tasks/webServer.js
+++
@@ -1,6 +0,0 @@
-var webServer = require('gulp-webserver');
-
-gulp.task('webServer', function () {
-    gulp.src(config['public'])
-        .pipe(webServer(config['webServer']['server']));
-});
