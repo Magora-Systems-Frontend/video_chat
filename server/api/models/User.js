420852b58b5221bc06b86d2d9f1792c12b0241d9
Glebcha <glebcha@gmail.com>
2015-08-27 17:21:20 +0600

    changed schema

--- server/api/models/User.js
+++ server/api/models/User.js
@@ -2,14 +2,14 @@
 
     'use strict';
 
-    var mongoose = require('mongoose');
-    var Schema = mongoose.Schema;
-
-    var userSchema = new Schema({
-        name: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        email: { type: String, required: true, unique: true }
-    });
+    var mongoose = require('mongoose'),
+        bcrypt = require('bcryptjs'),
+        Schema = mongoose.Schema,
+        userSchema = new Schema({
+            name: { type: String, required: true, unique: true },
+            password: { type: String, required: true },
+            email: { type: String, required: true, unique: true }
+        });
 
     userSchema.methods = {
         create: create,
@@ -19,7 +19,38 @@
     };
 
     userSchema.statics = {
-        findUnique: findUnique
+        checkPassword: checkPassword,
+        findUnique: findUnique,
+        list: list
+    };
+
+    userSchema.pre('save', function(next) {
+        var user = this;
+
+        if (!user.isModified('password')){
+            return next();
+        }
+
+        bcrypt.genSalt(8, function(err, salt) {
+            if (err){
+                return next(err);
+            } 
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err){
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    });
+
+    function checkPassword(password, userPassword, cb) {
+        bcrypt.compare(password, userPassword, function(err, isMatch) {
+            if (err) return cb(err);
+            cb(null, isMatch);
+        });
     };
 
     function create(user, cb){
@@ -33,6 +64,27 @@
 
     function findUnique(userName, email, cb){
         return User.findOne({ $and: {name: userName, email: email}});
+    }
+
+    function list(query, cb){
+        var queryObj = query || {};
+        User.aggregate({$match: queryObj}, 
+        { $sort: {_id: -1} },
+        {
+            $project: {
+                _id: 0,
+                id: '$_id',
+                name: 1,
+                email: 1
+            }
+        }, 
+        function(err, users){
+            console.log('LIST USERS ----->', err, users);
+            if(err){
+                cb(err, null);
+            }
+            cb(null, users);
+        });
     }
 
     function update(user){
